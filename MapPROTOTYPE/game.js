19224af4d43bdc49e5add5dce2b1ca9ab1dabc3d
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-07-31 14:16:16 +0300

    Changes to speed and map

--- MapPROTOTYPE/game.js
+++ MapPROTOTYPE/game.js
@@ -73,7 +73,7 @@ function update() {
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
-        player.body.velocity.x = -500;
+        player.body.velocity.x = -150;
         // scrolling the bg
         //bg.tilePosition.x +=2;
 
@@ -83,7 +83,7 @@ function update() {
         }
     }
     else if (cursors.right.isDown) {
-        player.body.velocity.x = 500;
+        player.body.velocity.x = 150;
         // scrolling the bg
         //bg.tilePosition.x -=2;
 
