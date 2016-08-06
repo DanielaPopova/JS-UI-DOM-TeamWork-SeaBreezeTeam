2a8e66a06076524995f9534e953d1a3eae720422
mkjordanov <mkjordanov@gmail.com>
2016-08-06 18:22:44 +0300

    Made Octocats smaller. Added more octocats

--- MapPROTOTYPE/game.js
+++ MapPROTOTYPE/game.js
@@ -82,7 +82,7 @@ function create() {
     //player = game.add.sprite(32, 32, 'player');
 
 //=======
-    //player = game.add.sprite(game.world.width - 1000, game.world.height - 200, 'player');
+    
     player = game.add.sprite(32, 32, 'player');
 //>>>>>>> .theirs
     
@@ -378,27 +378,31 @@ function CreateBadDudes() {
   //                    y   | x
     
      var startPosition = [[ 160,  650, 1300],
-                         [ 416, 1472, 1408,  192],
-                         [ 672,  128,  896],
-                         [ 992,  672, 1568],
-                         [1248,  672, 1376],
-                         [1504,  992,  928]];
+                          [ 416, 1472, 1408,  192],
+                          [ 672,  128,  896],
+                          [ 992,  672, 1568],
+                          [1248,  672, 1376],
+                          [1504,  992,  928],
+                          [2496,  704],
+                          [2816, 1312]];
     var endXPositon = [[1000, 1664],
-                       [1984, 960, 576],
+                       [1984,  960,  576],
                        [ 384, 1344],
                        [ 224, 1792],
                        [1056, 1760],
-                       [1248, 640]];
+                       [1248,  640],
+                       [ 960],
+                       [1824]];
     for (var y = 0; y < startPosition.length; y++) {
         for (var x = 1; x < startPosition[y].length; x++) {
              var octoCat = badDudes.create(startPosition[y][x], startPosition[y][0], 'octo-cat');
-             octoCat.scale.setTo(0.5, 0.5);
+             octoCat.scale.setTo(0.4, 0.4);
              game.physics.arcade.enable(octoCat);
              octoCat.body.gravity.y = 800;
              octoCat.body.collideWorldBounds = true;
              game.add.tween(octoCat).to({x: endXPositon[y][x-1]}, 3000, Phaser.Easing.Linear.None, true, 0, 1000, true);        
         }
-}
+    }
 //>>>>>>> .theirs
 }
 //<<<<<<< .mine
